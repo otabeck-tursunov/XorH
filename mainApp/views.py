1769317505fe54d9b715d79df7728303bc4ad03a
Otabek Tursunov <otabecktursunov@gmail.com>
2024-02-06 18:29:04 +0500

    second commit

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,7 +5,7 @@ from .models import *
 def index(request):
     search = request.GET.get('search')
     togrilar = TogriSoz.objects.filter(soz=search)
-    if togrilar:
+    if togrilar and search:
         togriSoz = togrilar[0]
         notogriSozlar = NotogriSoz.objects.filter(t_soz=togriSoz)
         context = {
@@ -13,4 +13,15 @@ def index(request):
             'notogriSozlar': notogriSozlar
         }
         return render(request, 'index.html', context)
+    elif search:
+        notogriSozlar = NotogriSoz.objects.filter(soz=search)
+        if notogriSozlar:
+            togriSoz = TogriSoz.objects.get(id=notogriSozlar[0].t_soz.id)
+            notogriSozlar = NotogriSoz.objects.filter(t_soz=togriSoz)
+            context = {
+                'togriSoz': togriSoz,
+                'notogriSozlar': notogriSozlar
+            }
+            return render(request, 'index.html', context)
     return render(request, 'index.html')
+
